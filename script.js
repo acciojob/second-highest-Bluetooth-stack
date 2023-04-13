4eb32ba651d5766ed78e8cc6c7ea4f6ef9aa43f8
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-14 00:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+			//Write your code here
+			if(arr.length===0 || arr.length===1){
+		        return -Infinity;
+		    } 
+		    let same = arr.reduce((prev, ele)=>{
+		        if(prev===ele){
+		            return ele;
+		        }
+		    })
+		    if(same===arr[0]){
+		        return -Infinity
+		    }
+			
+			let max = -Infinity;
+		    let ind = -1;
+		    let smax = -Infinity;
+		
+		    arr.forEach((element, index) => {
+		        if(element>max){
+		            max = element;
+		            ind = index;
+		        }
+		    });
+		    arr.forEach((element, index) => {
+		        if(smax!==max && ind!==index){
+		            if(element>smax){
+		                smax = element;
+		            }
+		        }
+		    });
+		    return smax;
+			
+		}
+
+		function Main() {
+			var n = prompt("Enter the number of elements");
+			var arr = [];
+			for (var i = 0; i < n; i++) {
+				arr[i] = prompt("Enter element " + (i+1));
+			}
+			alert(secondHighest(arr));
+		}
+		Main();
